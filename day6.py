a97b8dcd21e79d6f1500ffb94bbde66924661bab
Amin <amin.rad.1371@gmail.com>
2022-12-05 21:12:51 -0800

    Day 6 solution

---
+++ day6.py
@@ -0,0 +1,16 @@
+#with open("inputs/test.txt") as f:
+with open("inputs/day6.txt") as f:
+    content = f.read().strip()
+
+
+
+def find_start_index(index_size):
+    last = list(content[:index_size])
+    for i in range(index_size, len(content)):
+        if len(set(last)) < index_size:
+            last = last[1:] + [content[i]]
+        else:
+            return i
+
+part_one = find_start_index(4)
+part_two = find_start_index(14)
