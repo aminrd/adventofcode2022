aec599ce4398ef85bf65a2300ce68ec899db7db4
Amin <amin.rad.1371@gmail.com>
2022-12-09 10:40:10 -0800

    Fixing part two

--- day9.py
+++ day9.py
@@ -1,4 +1,4 @@
-#with open("inputs/test.txt") as f:
+# with open("inputs/test.txt") as f:
 with open("inputs/day9.txt") as f:
     lines = f.readlines()
 
@@ -10,6 +10,7 @@ directions = {
     "D": (1, 0),
     "U": (-1, 0)
 }
+
 
 def diag_dir(dst, pos):
     xt, yt = dst
@@ -40,13 +41,14 @@ def adj(p1, p2):
 def step(position, d):
     return [pos + di for pos, di in zip(position, d)]
 
+
 head = [0, 0]
 tail = [0, 0]
 visited = {(0, 0)}
 
 for d, count in moves:
     for _ in range(count):
-        #print(f"Head : {head}, Tail : {tail}")
+        # print(f"Head : {head}, Tail : {tail}")
         head = step(head, d)
         if not adj(head, tail):
             if head[0] == tail[0]:
@@ -55,7 +57,7 @@ for d, count in moves:
                 tail[0] += d[0]
             else:
                 ddir = diag_dir(head, tail)
-                final_dir = (d[0] + ddir[0], d[1]+ ddir[1])
+                final_dir = (d[0] + ddir[0], d[1] + ddir[1])
                 tail = step(tail, final_dir)
 
         visited.add(tuple(tail))
@@ -63,4 +65,61 @@ for d, count in moves:
 ans1 = len(visited)
 print(ans1)
 
+# Part two
 
+N = 10
+pos = [[0, 0] for _ in range(N)]
+visited_tail = {(0, 0)}
+
+
+def pprint(base, m, n, pos):
+    grid = [['.' for _ in range(n)] for _ in range(m)]
+    for i in range(len(pos)):
+        x, y = pos[i][0] + base, pos[i][1] + base
+        if i == 0:
+            grid[x][y] = "H"
+        else:
+            grid[x][y] = str(i)
+
+    for row in grid:
+        print("".join(row))
+
+
+for direction, count in moves:
+    for _ in range(count):
+        prev = pos[0]
+        pos[0] = step(pos[0], direction)
+
+        for k in range(1, N):
+            h, t = pos[k - 1], pos[k]
+
+            if adj(h, t):
+                break
+
+            if not adj(h, t):
+                d_h = abs(h[1] - t[1])
+                d_v = abs(h[0] - t[0])
+
+                if (d_v, d_h) == (2, 2):
+                    t[1] = h[1] - 1 if h[1] > t[1] else h[1] + 1
+                    t[0] = h[0] - 1 if h[0] > t[0] else h[0] + 1
+
+                elif (d_v, d_h) == (1, 2) or (d_v, d_h) == (2, 1):
+                    if d_v == 1:
+                        t[0] = h[0]
+                        t[1] = h[1] - 1 if h[1] > t[1] else h[1] + 1
+                    else:
+                        t[1] = h[1]
+                        t[0] = h[0] - 1 if h[0] > t[0] else h[0] + 1
+
+                elif h[0] == t[0]:
+                    t[1] = h[1] - 1 if h[1] > t[1] else h[1] + 1
+
+                elif h[1] == t[1]:
+                    t[0] = h[0] - 1 if h[0] > t[0] else h[0] + 1
+
+                pos[k] = t
+
+        visited_tail.add(tuple(pos[-1]))
+
+print(len(visited_tail))
