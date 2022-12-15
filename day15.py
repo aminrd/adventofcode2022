4ce2cce02988fae6e9796cd522f34db15876fdf6
Amin <amin.rad.1371@gmail.com>
2022-12-14 22:18:14 -0800

    Adding day 15 part two

--- day15.py
+++ day15.py
@@ -1,7 +1,8 @@
-import collections
+from tqdm import tqdm
+from collections import defaultdict
 
 DEBUG = 0
-PART_NUMBER = 1
+PART_NUMBER = 2
 input_file = "inputs/test.txt" if DEBUG else "inputs/day15.txt"
 with open(input_file) as f:
     lines = f.readlines()
@@ -27,7 +28,7 @@ for line in lines:
     pairs.append((s, b))
 
 qy = 10 if DEBUG == 1 else 2000000
-covered = collections.defaultdict(set)
+covered = defaultdict(set)
 
 
 def generate_points(d, at_y):
@@ -42,14 +43,45 @@ def generate_points(d, at_y):
 
     return points
 
+if PART_NUMBER == 1:
 
-for S, B in pairs:
-    d = man_dist(S, B)
+    for S, B in pairs:
+        d = man_dist(S, B)
 
-    for dx, dy in generate_points(d, qy - S[1]):
-        x = S[0] + dx
-        y = S[1] + dy
-        if (x, y) != B:
-            covered[y].add((x, y))
+        for dx, dy in generate_points(d, qy - S[1]):
+            x = S[0] + dx
+            y = S[1] + dy
+            if (x, y) != B:
+                covered[y].add((x, y))
 
-print(f"Answer one is {len(covered[qy])}")
+    print(f"Answer one is {len(covered[qy])}")
+
+# Part two
+def generate_range(d, given_y):
+    if abs(given_y) > d:
+        return None
+    x = abs(d - abs(given_y))
+    return -x, x
+
+def generate_ranges(Y):
+    ranges = []
+    for S, B in pairs:
+        d = man_dist(S, B)
+        r = generate_range(d, Y - S[1])
+        if r is None:
+            continue
+        ranges.append((S[0]+r[0], S[0]+r[1]))
+    return ranges
+
+Found = False
+MAX = 20 if DEBUG else 4000000
+for Y in tqdm(range(MAX+1), desc="Searching line : "):
+    if Found:
+        break
+    prev = 0
+    for x_start, x_end in sorted(generate_ranges(Y)):
+        if x_start > prev + 1:
+            print(f"Found : {(prev+1, Y)}")
+            break
+        else:
+            prev = max(prev, x_end)
