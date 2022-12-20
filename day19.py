4181918fb83e054098b11bd0ad707d4a636bd4e7
Amin <amin.rad.1371@gmail.com>
2022-12-20 14:29:34 -0800

    Adding day 19 part one

--- day19.py
+++ day19.py
@@ -1,5 +1,6 @@
 from tqdm import tqdm
-DEBUG = 1
+
+DEBUG = 0
 PART_NUMBER = 1
 input_file = "inputs/test.txt" if DEBUG else "inputs/day19.txt"
 with open(input_file) as f:
@@ -12,6 +13,7 @@ index = {
     "obsidian": 2,
     "geode": 3
 }
+
 
 class Robot:
     def __init__(self, line):
@@ -48,11 +50,38 @@ class Blueprint:
 MAX_TIME = 24
 blue_prints = [Blueprint(line) for line in lines]
 
-def get_best(b: Blueprint, t: int, robots: tuple, resources: tuple, best: int) -> int:
+
+# Generating one new ore robot each round, can generate parallel robots, remove ore dependency for last two robots
+def upperbound(b: Blueprint, t: int, robots: tuple, resources: tuple):
+    res = list(resources)
+    r = list(robots)
+    required = (0, b.robots[1].required[0], b.robots[2].required[1], b.robots[3].required[2])
+
+    for _ in range(MAX_TIME - t):
+        prev_r = tuple(r)
+
+        r[0] += 1
+        for i in range(1, 4):
+            if required[i] <= res[i - 1]:
+                res[i - 1] -= required[i]
+                r[i] += 1
+
+        for i in range(4):
+            res[i] += prev_r[i]
+
+    return res[-1]
+
+
+def get_best(b: Blueprint, t: int, robots: tuple, resources: tuple, best: int, visited_states: set) -> int:
     if t >= MAX_TIME:
         return max(best, resources[-1])
 
-    if t > 10 and resources[-1] + (MAX_TIME - t) * (robots[-1]+1) < best:
+    key = (*robots, *resources, t)
+    if key in visited_states:
+        return best
+
+    if upperbound(b, t, robots, resources) <= best:
+        visited_states.add(key)
         return best
 
     new_resources = tuple(resource + robot_cnt for resource, robot_cnt in zip(resources, robots))
@@ -61,17 +90,18 @@ def get_best(b: Blueprint, t: int, robots: tuple, resources: tuple, best: int) -
         if r.enough(resources):
             new_robots = list(robots)
             new_robots[ri] += 1
-            best = get_best(b, t + 1, tuple(new_robots), r.subtract(new_resources), best)
+            best = get_best(b, t + 1, tuple(new_robots), r.subtract(new_resources), best, visited_states)
 
     # Also do nothing:
-    best = get_best(b, t + 1, robots, new_resources, best)
+    best = get_best(b, t + 1, robots, new_resources, best, visited_states)
+    visited_states.add(key)
 
     return best
 
 
 total_sum = 0
 for bi, blue_print in tqdm(list(enumerate(blue_prints))):
-    best = get_best(blue_print, 0, (1, 0, 0, 0), (0, 0, 0, 0), 0)
-    total_sum += (bi+1) * best
+    best = get_best(blue_print, 0, (1, 0, 0, 0), (0, 0, 0, 0), 0, set())
+    total_sum += (bi + 1) * best
 
 print(f"Answer part one is {total_sum}")
