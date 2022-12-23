0752fe6fe69a950dcb76aee07fc19019346bf4c3
Amin <amin.rad.1371@gmail.com>
2022-12-23 13:33:32 -0800

    Solving day 22 part two

--- day22.py
+++ day22.py
@@ -2,7 +2,6 @@ from bisect import bisect_left
 from collections import defaultdict
 
 DEBUG = 0
-PART_NUMBER = 1
 input_file = "inputs/test.txt" if DEBUG else "inputs/day22.txt"
 with open(input_file) as f:
     lines = f.readlines()
@@ -138,16 +137,108 @@ real_cube_indexer = [
     [5, None, None]
 ]
 
+# (C_Dest, flip_indices?, Side_dst)
+# Side_src = [RDLU]
+debug_mapper = {
+    0: [(5, True, 'R'), (3, False, 'U'), (2, False, 'U'), (1, True, 'U')],
+    1: [(2, False, 'L'), (4, True, 'D'), (5, True, 'D'), (0, True, 'U')],
+    2: [(3, False, 'L'), (4, True, 'L'), (1, False, 'R'), (0, False, 'L')],
+    3: [(5, True, 'U'), (4, False, 'U'), (2, False, 'R'), (0, False, 'D')],
+    4: [(5, False, 'L'), (1, True, 'D'), (2, True, 'D'), (3, False, 'D')],
+    5: [(0, True, 'R'), (1, True, 'L'), (4, False, 'R'), (3, True, 'R')],
+}
+real_mapper = {
+    0: [(1, False, 'L'), (2, False, 'U'), (3, True, 'L'), (5, False, 'L')],
+    1: [(4, True, 'R'), (2, False, 'R'), (0, False, 'R'), (5, False, 'D')],
+    2: [(1, False, 'D'), (4, False, 'U'), (3, False, 'U'), (0, False, 'D')],
+    3: [(4, False, 'L'), (5, False, 'U'), (0, True, 'L'), (2, False, 'L')],
+    4: [(1, True, 'R'), (5, False, 'R'), (3, False, 'R'), (2, False, 'D')],
+    5: [(4, False, 'D'), (1, False, 'U'), (0, False, 'U'), (3, False, 'D')],
+}
+
+
+def get_node_index_and_direction(ind, flip, entrance):
+    index = N - ind - 1 if flip else ind
+    if entrance == 'R':
+        return (index, N - 1), 'L'
+    elif entrance == 'D':
+        return (N - 1, index), 'U'
+    elif entrance == 'L':
+        return (index, 0), 'R'
+    else:
+        return (0, index), 'D'
+
+
+def get_next_node(c_src, i, j):
+    mapper = debug_mapper[c_src] if DEBUG else real_mapper[c_src]
+
+    if j == N:  # Right exit
+        c_next, flip, entrance = mapper[0]
+        index, direction = get_node_index_and_direction(i, flip, entrance)
+    elif i == N:  # Down exit
+        c_next, flip, entrance = mapper[1]
+        index, direction = get_node_index_and_direction(j, flip, entrance)
+    elif j == -1:  # Left exit
+        c_next, flip, entrance = mapper[2]
+        index, direction = get_node_index_and_direction(i, flip, entrance)
+    else:  # Top exit
+        c_next, flip, entrance = mapper[3]
+        index, direction = get_node_index_and_direction(j, flip, entrance)
+
+    return c_next, index, direction
+
+
 ci, I, J = 0, 0, 0
 dy, dx = 0, 1
+direction_mapper = {
+    'R': (0, 1),
+    'D': (1, 0),
+    'L': (0, -1),
+    'U': (-1, 0)
+}
+direction_point = {
+    (0, 1): 0,
+    (1, 0): 1,
+    (0, -1): 2,
+    (-1, 0): 3
+}
+rotate_right = {
+    (0, 1): (1, 0),
+    (1, 0): (0, -1),
+    (0, -1): (-1, 0),
+    (-1, 0): (0, 1)
+}
+rotate_left = {
+    (0, 1): (-1, 0),
+    (1, 0): (0, 1),
+    (0, -1): (1, 0),
+    (-1, 0): (0, -1)
+}
 
 for rotate, move in instructions:
     for _ in range(move):
-        ai, aj = I+dy, J+dx
+        ai, aj = I + dy, J + dx
         if 0 <= ai < N and 0 <= aj < N:
             if cube[ci][ai][aj].blocked():
                 break
             else:
                 I, J = ai, aj
         else:
-            pass
+            c_next, (ai, aj), direction = get_next_node(ci, ai, aj)
+            if cube[c_next][ai][aj].blocked():
+                break
+            else:
+                ci, I, J = c_next, ai, aj
+                dy, dx = direction_mapper[direction]
+
+    if rotate == "R":
+        dy, dx = rotate_right[(dy, dx)]
+    elif rotate == "L":
+        dy, dx = rotate_left[(dy, dx)]
+    else:
+        break
+
+node = cube[ci][I][J]
+final_i, final_j = node.position()
+password = 1000 * (final_i + 1) + 4 * (final_j + 1) + direction_point[(dy, dx)]
+print(f"Answer part two is {password}")
